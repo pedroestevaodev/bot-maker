40945b841728b14c108f1ebc01236d25427750aa
Wizard-Anorak <pedropaulista11@gmail.com>
2021-01-17 17:49:11 -0300

    feat: add Watson natural language understanting service

--- credentials/README.md
+++ credentials/README.md
@@ -9,3 +9,18 @@ File: `algorithmia.json`
     "apiKey": "simSw3lZuHN/EzvG2IQMscAMr+41"
 }
 ```
+
+## Watson Natural Language Understanding
+
+File: `watson-nlu.json`
+
+```
+{
+    "apikey": "bcuyIOyrO8dm29c63HJTM2Ov_lydsz6CyqxNyQSSzYjS",
+    "iam_apikey_description": "Auto-generated for key ce64f470-8933-450b-b229-a9adb1807655",
+    "iam_apikey_name": "Auto-generated service credentials",
+    "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Manager",
+    "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/db0c717e5ac04be1aa10e7e75e68e630::serviceid:ServiceId-c1e683ad-2b60-4a12-87f6-c3928022b9e9",
+    "url": "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/56def2ef-5b47-4248-bfec-4f7681063b82"
+}
+```
--- robots/text.js
+++ robots/text.js
@@ -1,17 +1,27 @@
 const algorithmia = require('../node_modules/algorithmia')
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
 const sentenceBoundaryDetection = require('../node_modules/sbd')
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+const NaturalLanguageUnderstandingV1 = require('../node_modules/watson-developer-cloud/natural-language-understanding/v1.js')
+
+var nlu = new NaturalLanguageUnderstandingV1({
+  iam_apikey: watsonApiKey,
+  version: '2018-04-05',
+  url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+})
 
 async function robot(content) {
     await fetchContentFromWikipedia(content)
     sanitizeContent(content)
     breakContentIntoSentences(content)
+    limitMaximumSentences(content)
+    await fetchKeywordsOfAllSentences(content)
 
     async function fetchContentFromWikipedia(content) {
         const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
         const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
-        const wikipediaResponde = await wikipediaAlgorithm.pipe(content.searchTerm)
-        const wikipediaContent = wikipediaResponde.get()
+        const wikipediaResponse = await wikipediaAlgorithm.pipe(content.searchTerm)
+        const wikipediaContent = wikipediaResponse.get()
 
         content.sourceContentOriginal = wikipediaContent.content
         process.on("unhandledRejection", err => {
@@ -57,6 +67,36 @@ async function robot(content) {
         //console.log(sentences) 
         process.on("unhandledRejection", err => {
             console.log(`Problema ao organizar sentenças do texto. Erro: ${err.stack}`)
+        })
+    }
+
+    function limitMaximumSentences(content) {
+        content.sentences = content.sentences.slice(0, content.maximumSentences)
+    }
+
+    async function fetchKeywordsOfAllSentences(content) {
+        for (const sentence of content.sentences) {
+            sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+        }
+    }
+
+    async function fetchWatsonAndReturnKeywords(sentence) {
+        return new Promise((resolve, reject) => {
+            nlu.analyze({
+                text: sentence,
+                features: {
+                    keywords: {}
+                }
+            }, (error, response) => {
+                if(error) {
+                    throw error
+                }
+            
+                const keywords = response.keywords.map((keyword) => {
+                    return keyword.text
+                })
+                resolve(keywords)
+            })
         })
     }
 }
