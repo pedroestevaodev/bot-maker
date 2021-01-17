a6064bdf7043a1a22592bf06e2ea00d9afa9d1ab
Wizard-Anorak <pedropaulista11@gmail.com>
2021-01-17 16:04:25 -0300

    feat: start text robot

---
+++ credentials/README.md
@@ -0,0 +1,11 @@
+# Credentials format
+
+## Algorithmia
+
+File: `algorithmia.json`
+
+```
+{
+    "apiKey": "simSw3lZuHN/EzvG2IQMscAMr+41"
+}
+```
--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const readline = require('readline-sync')
+const robots = {
+    text: require('./robots/text.js')
+}
 
-function start() {
+async function start() {
     const content = {}
 
     content.searchTerm = askAndReturnSearchTerm()
     content.prefix = askAndReturnPrefix()
+
+    await robots.text(content)
 
     function askAndReturnSearchTerm() {
         return readline.question('Type a Wikipedia search term: ')
     }
     function askAndReturnPrefix() {
         const prefixes = ['Who is','What is','The history of']
-        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ') 
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
         const selectedPrefixText = prefixes[selectedPrefixIndex]
 
         return selectedPrefixText
